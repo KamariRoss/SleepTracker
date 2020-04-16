c9a476b15f5ac551f2ef89fd70d433cd70530e89
KamariRoss <kamarimobile@gmail.com>
2020-04-15 20:32:28 -0400

    Started in SleepQuality page

--- ClientApp/src/pages/SleepQuality.jsx
+++ ClientApp/src/pages/SleepQuality.jsx
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { useCallback, useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import axious from 'axios'
+import { Redirect } from 'react-router-dom'
 
-const SleepQuality = () => {
+const SleepQuality = props => {
+  const sleepId = props.match.params.SleepCounterId
+  const getSleepStats = async () => {
+    const resp = await axious.get(`/api/Sleep/${sleepId}`)
+    console.log(resp.data)
+  }
+
+  useEffect(() => {
+    getSleepStats()
+  }, [])
+
+  const goodRating = async () => {
+    const resp2 = await axious.put(`/api/Sleep/${sleepId}/quality`)
+  }
+
   return (
-    <div class="sleepQualityPage">
-      <h1 class="sleepQualityTitle">How was your sleep quality</h1>
-      <section class="sleepQualityButton">
-        <Link to="/">
-          <button class="simleyFace"> Test</button>
-          <button class="simleyFace"> Test</button>
-          <button class="simleyFace"> Test</button>
-        </Link>
+    <div className="sleepQualityPage">
+      <h1 className="sleepQualityTitle">How was your sleep quality?</h1>
+      <section className="sleepQualityButton">
+        <button className="simleyFace" onClick={goodRating}>
+          {' '}
+          Test
+        </button>
+        <button className="simleyFace" onClick={okayRating}>
+          {' '}
+          Test
+        </button>
+        <button className="simleyFace" onClick={badRating}>
+          {' '}
+          Test
+        </button>
       </section>
     </div>
   )
